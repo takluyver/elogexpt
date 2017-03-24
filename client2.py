4a80b4c26dffdba45afdfdc75471ceb4a9aed303
Thomas Kluyver <thomas@kluyver.me.uk>
2017-03-24 15:06:48 +0000

    Working live plot and snapshots

--- client2.py
+++ client2.py
@@ -1,13 +1,21 @@
+from base64 import encodebytes
+from collections import deque
 import json
+import matplotlib.pyplot as plt
 from pathlib import Path
 import sqlite3
 from subprocess import Popen, PIPE
 import sys
 from tornado.ioloop import IOLoop
+import traceback
 
 from IPython.display import display, Javascript
 
 mydir = Path(__file__).parent
+
+dbconn = sqlite3.connect('example.sqlite')
+dbconn.execute('''CREATE TABLE IF NOT EXISTS thedata
+        (id integer primary key autoincrement, a integer, b integer)''')
 
 class DataReceiver:
     def __init__(self, pipe, loop=None):
@@ -15,14 +23,11 @@ class DataReceiver:
         self.loop = loop or IOLoop.current()
         self.callbacks = []
         self.buffer = b''
-        self.dbconn = sqlite3.connect('example.sqlite')
-        self.dbconn.execute('''CREATE TABLE IF NOT EXISTS thedata
-                (id integer primary key autoincrement, a integer, b integer)''')
         self.loop.add_handler(pipe, self.read_data, IOLoop.READ)
     
     def add_to_db(self, j):
-        with self.dbconn:
-            cursor = self.dbconn.execute('INSERT INTO thedata VALUES (NULL, ?, ?)',
+        with dbconn:
+            cursor = dbconn.execute('INSERT INTO thedata VALUES (NULL, ?, ?)',
                                     (j['a'], j['b']))
             j['id'] = cursor.lastrowid
 
@@ -40,6 +45,47 @@ class DataReceiver:
             for cb in self.callbacks:
                 cb(d)
 
+def plotit(data):
+    ids = [d['id'] for d in data]
+    a = [d['a'] for d in data]
+    b = [d['b'] for d in data]
+
+    plt.plot(ids, a, label='a')
+    plt.plot(ids, b, label='b')
+    plt.ylim(0, 20)
+    plt.xlim(ids[0], ids[-1])
+    plt.legend()
+
+def get_range(from_id, to_id):
+    cur = dbconn.execute('''SELECT * FROM thedata WHERE (id BETWEEN ? AND ?)
+                    ORDER BY id''', (from_id, to_id))
+    return [{'id': r[0], 'a': r[1], 'b': r[2]} for r in cur]
+
+def show_range(from_id, to_id):
+    data = get_range(from_id, to_id)
+    plotit(data)
+    plt.show()
+    plt.close()
+
+class LivePlotter:
+    def __init__(self):
+        # self.comm = comm
+        self.format = get_ipython().display_formatter.format
+        self.recent = deque(maxlen=20)
+    
+    def update(self, datum):
+        # self.comm.send(datum)
+        try:
+            self.recent.append(datum)
+            plotit(self.recent)
+            data, metadata = self.format(plt.gcf())
+            self.comm.send({'png': encodebytes(data['image/png']),
+                            'last_id': datum['id']})
+            plt.close()
+        except Exception as e:
+            self.comm.send(str(e))
+            traceback.print_exc(file=sys.__stderr__)
+
 def init_notebook():
     with (mydir / 'commclient.js').open() as f:
         js_code = f.read()
@@ -47,12 +93,10 @@ def init_notebook():
     producer = Popen([sys.executable, 'producer.py'], stdout=PIPE)
     receiver = DataReceiver(producer.stdout)
 
+    plotter = LivePlotter()
     def comm_opened(comm, msg):
-        
-        def data_recvd(datum):
-            comm.send(datum)
-
-        receiver.callbacks.append(data_recvd)
+        plotter.comm = comm
+        receiver.callbacks.append(plotter.update)
 
     get_ipython().kernel.comm_manager.register_target('elogexpt', comm_opened)
 
--- commclient.js
+++ commclient.js
@@ -1,8 +1,34 @@
-var comm = Jupyter.notebook.kernel.comm_manager.new_comm('elogexpt')
+var comm = Jupyter.notebook.kernel.comm_manager.new_comm('elogexpt');
+console.log(comm);
 
-console.log(comm)
+var imgelt = $('<img/>').appendTo(element);
+var last_id;
 
 // Register a handler
 comm.on_msg(function(msg) {
-    console.log(msg.content.data);
+    var data = msg.content.data;
+    if (data.png) {
+      imgelt[0].src = 'data:image/png;base64,'+ msg.content.data.png;
+      last_id = data.last_id;
+    } else {
+      console.log(data);
+    }
 });
+
+function add_snapshot_now(){
+    var start_id = last_id - 20;
+    if (start_id < 0) {
+      start_id = 0;
+    }
+    var range = "(" + start_id + ", " + last_id + ")"
+    var new_cell = Jupyter.notebook.insert_cell_below();
+    new_cell.code_mirror.setValue("data = client2.show_range" + range);
+    new_cell.execute();
+}
+
+Jupyter.toolbar.add_buttons_group([{
+    label: 'Experiment state summary now',
+    icon: 'fa-umbrella',
+    callback: add_snapshot_now,
+    id: 'add_summary_button'
+}]);
